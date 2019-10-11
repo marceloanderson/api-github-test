aecb5c83541f5d77e4d107b2e7c5bd694e7bffcc
Marcelo Anderson <marceloanderson84@gmail.com>
2019-10-11 13:05:48 -0300

    teste com ajax e KO

--- src/assets/scripts/general.js
+++ src/assets/scripts/general.js
@@ -5,14 +5,50 @@ _app.general = {
         console.log("init general");
 
         ko.applyBindings(new _app.general.AppViewModel());
+        // ko.applyBindings(new _app.general.searchUserGH());
     }, 
     AppViewModel() {
         this.firstName = ko.observable("Bert");
-        this.lastName = ko.observable("Bertington");
+        this.lastName = ko.observable("");
         
         this.fullName = ko.computed(function() {
-            return this.firstName() + " " + this.lastName();    
+            // return this.firstName() + " " + this.lastName();   
+            
+            $.ajax({
+                url: "https://api.github.com/users/" + this.lastName(),
+                type: "GET",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function(data) {
+                    console.log(data);
+                }, 
+                error: function(){
+                    console.log("erro");
+                }
+            });
+
         }, this);
+    },
+    searchUserGH(){
+        // const endPointUser = "https://api.github.com/users/";
+        // this.userGithub = ko.observable("Teste");
+
+        // let userGH = ko.computed(function() {
+        //     return this.userGithub();    
+        // }, this);
+
+        // $.ajax({
+        //     url: endPointUser + userGH,
+        //     type: "GET",
+        //     dataType: "json",
+        //     contentType: "application/json; charset=utf-8",
+        //     success: function(data) {
+        //         console.log(data);
+        //     }, 
+        //     error: function(){
+        //         console.log("erro");
+        //     }
+        // });
     },
     initLoad(){
         console.log("initLoad general");
