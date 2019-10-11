3c013b324858f99ed007bddfb9f0f00f00b18e4c
Marcelo Anderson <marceloanderson84@gmail.com>
2019-10-11 12:20:57 -0300

    estruturando html, css, js

--- src/assets/scripts/general.js
+++ src/assets/scripts/general.js
@@ -3,7 +3,17 @@ const _app = window._app || {};
 _app.general = {
     init(){
         console.log("init general");
+
+        ko.applyBindings(new _app.general.AppViewModel());
     }, 
+    AppViewModel() {
+        this.firstName = ko.observable("Bert");
+        this.lastName = ko.observable("Bertington");
+        
+        this.fullName = ko.computed(function() {
+            return this.firstName() + " " + this.lastName();    
+        }, this);
+    },
     initLoad(){
         console.log("initLoad general");
     }
