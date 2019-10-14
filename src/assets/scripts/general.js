f0aab50eba20deb5da557af44b48a5d1514ecd43
Marcelo Anderson <marceloanderson84@gmail.com>
2019-10-14 17:17:26 -0300

    search page

--- src/assets/scripts/general.js
+++ src/assets/scripts/general.js
@@ -1,68 +1,47 @@
-const _app = window._app || {};
 
-_app.general = {
-    init(){
-        console.log("init general");
+function searchUser(userName){
+    const user = userName;
+    const endPointUser = "https://api.github.com/search/users?q=";
 
-        ko.applyBindings(new _app.general.AppViewModel());
-        // ko.applyBindings(new _app.general.searchUserGH());
-    }, 
-    AppViewModel() {
-        this.firstName = ko.observable("Bert");
-        this.lastName = ko.observable("");
-        
-        this.fullName = ko.computed(function() {
-            // return this.firstName() + " " + this.lastName();   
+    $.ajax({
+        url: endPointUser + user,
+        type: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        beforeSend: function(data) {
             
-            $.ajax({
-                url: "https://api.github.com/users/" + this.lastName(),
-                type: "GET",
-                dataType: "json",
-                contentType: "application/json; charset=utf-8",
-                success: function(data) {
-                    console.log(data);
-                }, 
-                error: function(){
-                    console.log("erro");
-                }
-            });
+        },
+        success: function(data) {
+            console.log(data);
+            console.log(data.items.length);
+            
+        }, 
+        error: function(){
+            console.log("erro");
+        }
+    });
+}
 
-        }, this);
-    },
-    searchUserGH(){
-        // const endPointUser = "https://api.github.com/users/";
-        // this.userGithub = ko.observable("Teste");
+function usersViewModel(){
+    console.log("usersViewModel");
 
-        // let userGH = ko.computed(function() {
-        //     return this.userGithub();    
-        // }, this);
+    this.userName = ko.observable("");
 
-        // $.ajax({
-        //     url: endPointUser + userGH,
-        //     type: "GET",
-        //     dataType: "json",
-        //     contentType: "application/json; charset=utf-8",
-        //     success: function(data) {
-        //         console.log(data);
-        //     }, 
-        //     error: function(){
-        //         console.log("erro");
-        //     }
-        // });
-    },
-    initLoad(){
-        console.log("initLoad general");
-    }
+    ko.computed(function() {
+        if(this.userName() != ""){
+            searchUser(this.userName())
+        }
+    }, this);
 }
 
 $(function() {
     "use strict";
 
     // READY
-    _app.general.init();
+    ko.applyBindings(new usersViewModel());
 
     // LOAD
     $(window).load(function () {
-        _app.general.initLoad();
+        // 
     });
 });
